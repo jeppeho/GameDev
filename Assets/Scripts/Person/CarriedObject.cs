46a9f482d4aaead8babd675023488965c91c72a4
Jeppeh Olsen <jeppeholsen@a.local>
2016-03-26 16:09:51 +0100

    Created separate relic script and new prototype scene with updated prefabs

--- Assets/Scripts/Person/CarriedObject.cs
+++ Assets/Scripts/Person/CarriedObject.cs
@@ -22,169 +22,91 @@ public class CarriedObject : MonoBehaviour {
 		//Get state of player
 		playerState = this.gameObject.GetComponent<PlayerManager> ().GetState ();
 
-		//Check if carried object contains an object
-		if (carriedObject != null) {
+		//If player dies, release object
+		if (playerState == "dead") {
 
-			//Update rigidbody of carried object
-			rb = carriedObject.gameObject.GetComponent<Rigidbody> ();
-
-			//If not dropping game object
-			if (dropping == false) {
-
-				//Check if player is dead
-				if (playerState == "dead") {
-
-					StartCoroutine (DropObjectInCenter ());
-				
-				} else {
-
-					//Update position
-					slotPosition = this.transform.position;
-					slotPosition.y += 1f;
-					carriedObject.transform.position = slotPosition;
-				}
-
-				//If key is pressed
-				if (Input.GetKey ("p")) {
-					StartCoroutine(DropObjectInCenter ());
-				}	
+			if (carriedObject != null) {
+				ReleaseCarriedObject ();
 			}
 		}
 	}
 
 
 	void OnCollisionEnter(Collision collision){
+		//Debug.Log ("Running OnCollisionEnter() in CarriedObject script");
 
 		if (playerState == "active") {
-			if (collision.gameObject.tag == "Relic") {
 
-				//Set relic as carriedObject
-				carriedObject = GameObject.FindGameObjectWithTag ("Relic");
+			if (carriedObject == null) {
 
-				//Set player as parent to Relic
-				carriedObject.transform.SetParent (this.gameObject.transform, false);
+				if (collision.gameObject.tag == "Relic") {
 
-				//Freeze relic rotation
-				rb.freezeRotation = true;
+					//Set relic as carriedObject
+					carriedObject = collision.gameObject;
+
+					//Set player as parent to Relic
+					carriedObject.GetComponent<RelicController>().SetParent(this.gameObject.transform);
+				}
 			}
 		}
 	}
+
+	public void ReleaseCarriedObject(){
+		carriedObject.GetComponent<RelicController>().RemoveParent();
+		carriedObject = null;
+	}
 		
-
-	void DropObject(){
-
-		//add value to z-position
-		slotPosition.z += 2f;
-
-		//Set it a bit forward into scene
-		carriedObject.transform.position = slotPosition;
-
-		//Remove parent object
-		carriedObject.transform.parent = null;
-
-		//Scale up (it has been scaled down when picking up)
-		carriedObject.transform.localScale /= 0.3f;
-
-		//Remove carried object
-		carriedObject = null;
-	}
-
-	IEnumerator DropObjectInCenter(){
-
-		dropping = true;
-
-		//Unfreeze relic rotation
-		rb.freezeRotation = false;
-
-		//Position of the carried object
-		Vector3 position = rb.transform.position;
-
-		//Move carried object upwards
-		while (position.y < 0f) {
-			position = rb.transform.position;
-			rb.AddForce ( new Vector3(0, 50f, 0));
-			Debug.Log ("Relic.y = " + position.y);
-			yield return new WaitForSeconds (0f);
-			
-		}
-
-		int frame = 0;
-		//Create vector from current position towards center
-		position = rb.transform.position;
-
-		//Get target position for dropped carried object
-		Vector3 target = new Vector3 (5, position.y, position.z + 5f);
-
-		//Get the vector towards the target
-		Vector3 vectorToTarget = (target - position);
-		vectorToTarget.Normalize ();
-		vectorToTarget *= 50;
-
-		//Move carried object towards target position
-		while(frame < 10){
-			rb.AddForce( vectorToTarget );
-			yield return new WaitForSeconds (0f);
-			frame += 1;
-		}
-
-
-		//Remove parent object
-		carriedObject.transform.parent = null;
-
-		//Scale up (it has been scaled down when picking up)
-		carriedObject.transform.localScale /= 0.3f;
-
-		//Remove carried object
-		carriedObject = null;
-
-		dropping = false;
-	}
-
-
-	IEnumerator DropObjectInCenterNEW(){
-
-		dropping = true;
-
-		//Unfreeze relic rotation
-		rb.freezeRotation = false;
-
-		//Create vector from current position towards center
-		Vector3 position = rb.transform.position;
-		Vector3 center = position;
-		center.x = 5;
-		center.y = position.y + 5f;
-		center.z = position.z + 5f;
-
-		Vector3 directionToCenter = (center - position);
-		Debug.Log ("1. CENTER vec = " + directionToCenter);
-		directionToCenter.Normalize ();
-		Debug.Log ("2. CENTER vec = " + directionToCenter);
-		directionToCenter *= 30;
-
-
-		while (rb.transform.position.y < 5f) {
-			rb.AddForce( new Vector3(0,3f,0));
-			position = rb.transform.position;
-			yield return new WaitForSeconds (0f);
-		}
-
-		//Move carried object towards center
+	/**
+	 * Not in use anymore...
+	 */
+//	IEnumerator DropObjectInCenter(){
+//
+//		dropping = true;
+//
+//		//Unfreeze relic rotation
+//		rb.freezeRotation = false;
+//
+//		//Position of the carried object
+//		Vector3 position = rb.transform.position;
+//
+//		//Move carried object upwards
+//		while (position.y < 0f) {
+//			position = rb.transform.position;
+//			rb.AddForce ( new Vector3(0, 50f, 0));
+//			Debug.Log ("Relic.y = " + position.y);
+//			yield return new WaitForSeconds (0f);
+//			
+//		}
+//
 //		int frame = 0;
-//		while (frame < 10) {
-//			rb.AddForce( directionToCenter );
+//		//Create vector from current position towards center
+//		position = rb.transform.position;
+//
+//		//Get target position for dropped carried object
+//		Vector3 target = new Vector3 (5, position.y, position.z + 5f);
+//
+//		//Get the vector towards the target
+//		Vector3 vectorToTarget = (target - position);
+//		vectorToTarget.Normalize ();
+//		vectorToTarget *= 50;
+//
+//		//Move carried object towards target position
+//		while(frame < 10){
+//			rb.AddForce( vectorToTarget );
 //			yield return new WaitForSeconds (0f);
 //			frame += 1;
 //		}
-
-		//Remove parent object
-		carriedObject.transform.parent = null;
-
-		//Scale up (it has been scaled down when picking up)
-		carriedObject.transform.localScale /= 0.3f;
-
-		//Remove carried object
-		carriedObject = null;
-
-		dropping = false;
-	}
+//
+//
+//		//Remove parent object
+//		carriedObject.transform.parent = null;
+//
+//		//Scale up (it has been scaled down when picking up)
+//		carriedObject.transform.localScale /= 0.3f;
+//
+//		//Remove carried object
+//		carriedObject = null;
+//
+//		dropping = false;
+//	}
 }
--- Assets/Scripts/Relic/RelicController.cs
+++ Assets/Scripts/Relic/RelicController.cs
@@ -3,16 +3,167 @@ using System.Collections;
 
 public class RelicController : MonoBehaviour {
 
-	Vector3 targetPosition;
+	//private Vector3 targetPosition;
+	private Rigidbody rb;
+	private Transform parent;
+
+	private bool movingToTarget;
+
+
+	//Bounds - should be put in a LevelManager script
+	float minX, maxX, minY, minZ;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		rb = this.gameObject.GetComponent<Rigidbody> ();
+		UpdateFreezeRotation (false);
+		movingToTarget = false;
+
+		minX = 0; maxX = 10.5f; minY = 0; minZ = -150; //Bounds - should be put in a LevelManager script
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (GetParent() == null) {
+
+			if (!IsWithinBounds ()) {
+
+				if (GetPosition ().y < 1f) {
+				
+					rb.AddForce (new Vector3 (0, 40f, 0));
+
+				} else {
+					
+					if (movingToTarget == false) {
+						//movingToTarget = true;
+						StartCoroutine (MoveTowardsCenter ());
+					}
+				}
+			}
+		} else {
+			//Follow parent
+			FlyAboveParent ();
+		}
+	}
+
+	/**
+	 * Check if the relic is within the bounds of the level
+	 */
+	public bool IsWithinBounds(){
+		if (GetPosition ().x > maxX || GetPosition ().x < minX || GetPosition ().y < minY || GetPosition().z < minZ)
+			return false;
+		else
+			return true;
+	}
+
+	/*
+	 * Returns true if the relic is below ground level, otherwise false
+	 */
+	public bool IsBelowGround(){
+		if (GetPosition ().y < 0)
+			return true;
+		else
+			return false;
+	}
+
+	//Updates the position to be above the parents head
+	private void FlyAboveParent(){
+		Vector3 slotPosition = GetParent().position;
+		slotPosition.y += 2f;
+		UpdatePosition( slotPosition );
+	}
+
+
+	/**
+	 * Moves the relic to the center of the level and a bit forward
+	 */
+	IEnumerator MoveTowardsCenter(){
+
+		//Get target position for dropped carried object
+		Vector3 target = new Vector3 (5, GetPosition ().y, GetPosition ().z + 3f);
+
+		Vector3 vectorToTarget = target - GetPosition (); 
+		vectorToTarget.Normalize ();
 		
+		//Move carried object towards target position
+		while(!IsWithinBounds()){
+			rb.AddForce( vectorToTarget );
+		
+			yield return new WaitForSeconds (0f);
+		}
+
+		if (IsWithinBounds()) {
+			movingToTarget = false;
+		}
+	}
+		
+
+	/**
+	 * If distance to parent is over some maximum
+	 */
+	public void ReleaseFromParent(){
+		
+	}
+
+	/**
+	 * Updates the freezeRotation parameter, with the inputted bool value
+	 */
+	public void UpdateFreezeRotation(bool rotate){
+		rb.freezeRotation = rotate;
+	}
+
+	/**
+	 * Set current rotation to zero
+	 */
+	public void ResetRotation(){
+		rb.transform.localRotation = new Quaternion();
+	}
+
+	/**
+	 * Set a parent to the relic
+	 * Minimize the scale
+	 * Freeze rotation
+	 * And resets rotation to zero.
+	 */
+	public void SetParent(Transform parent){
+		this.transform.SetParent (parent, false);
+		UpdateScale (0.8f);
+		UpdateFreezeRotation (true);
+		ResetRotation ();
+	}
+
+	public void UpdateScale(float scale){
+		rb.transform.localScale = new Vector3 (scale, scale, scale);
+	}
+
+
+	public float GetScale(){
+		return rb.transform.localScale.x;
+	}
+
+
+	/**
+	 * Sets the parent variable to null,
+	 * updates the scale
+	 * And unfreezes the rotation
+	 */
+	public void RemoveParent(){
+		this.transform.parent = null;
+		UpdateScale (1f);
+		UpdateFreezeRotation (false);
+	}
+
+	public Transform GetParent(){
+		return this.transform.parent;
+	}
+
+	public void UpdatePosition(Vector3 position){
+		rb.transform.position = position;
+	}
+
+	public Vector3 GetPosition (){
+		return rb.transform.position;
 	}
 }
