0f75287e9a111573e3e4a3c7820ae5337ed7de87
Jeppeh Olsen <jeppeholsen@a.local>
2016-03-28 14:06:52 +0200

    Updated Minion controller script

--- Assets/Scripts/Person/NewController.cs
+++ Assets/Scripts/Person/NewController.cs
@@ -21,9 +21,11 @@ public class NewController : MonoBehaviour {
 	private bool isJumping = false;
 
 	public float accelerationRate;
+	public float maxAcceleration;
 	public float maxVelocity;
+	public float jumpPower;
+
 	int falldownCounter = 0;
-	private int numJumpFrames = 20;
 
 
 	// Use this for initialization
@@ -40,6 +42,7 @@ public class NewController : MonoBehaviour {
 		//Multiply with the game speed
 		maxVelocity *= LevelManager.SPEED;
 		accelerationRate *= LevelManager.SPEED;
+		maxAcceleration *= LevelManager.SPEED;
 	}
 	
 	// Update is called once per frame
@@ -47,33 +50,31 @@ public class NewController : MonoBehaviour {
 
 		GetInputButtonValues ();
 
-		//Set power if push button is pushed
-//		if (pressPush)
-//			SetPower (16, 8);
-//		else
-//			SetPower (10, 5);
-
-
 		UpdateDirection ();
 
+		//Check if player should throw relic
 		if (pressThrow)
 			ThrowRelic ();
 
+		//Move on X and Z axis
 		if(moveHorizontal != 0 || moveVertical != 0)
 			Move ();
 
+		//Check if player should fall or land
 		if (!IsGrounded ()) {
-			falldownCounter = 3;
+			FallDown ();
+		}else{
+			Landing();
 		}
-
-		FallDown ();
-
+			
+		//Check if player is jumping
 		if (pressJump && !isJumping) {
 			isJumping = true;
 			Jump2 ();
 			//StartCoroutine (Jump ());
 		}
 	}
+
 
 	/**
 	 * Throws the relic and removes it as child
@@ -117,39 +118,29 @@ public class NewController : MonoBehaviour {
 
 		Vector3 force = new Vector3 (hor, 0, ver);
 
+		if (force.magnitude > maxAcceleration)
+			force = Vector3.Normalize (force) * maxAcceleration;
+
 		//Add force
 		rb.AddForce (force * Time.deltaTime);
-
 
 
 		//Make vector to check ground velocity
 		Vector2 ground_speed = new Vector2 (rb.velocity.x, rb.velocity.z);
 
 		//If magnitude of x and z velocity is exceeded, then stomp it
-		if (ground_speed.magnitude > maxVelocity) {
-			ground_speed = Vector3.Normalize (ground_speed) * maxVelocity;
+		if (ground_speed.magnitude > GetMaxVelocity()) {
 
+			//Set speed to the maxVelocity
+			ground_speed = Vector3.Normalize (ground_speed) * GetMaxVelocity();
+
+			//Convert to 3 dimension vector
 			Vector3 updatedVel = new Vector3 (ground_speed.x, rb.velocity.y, ground_speed.y);
 			rb.velocity = updatedVel;
 		}
+
+
 	}
-
-	private void FallDown(){
-		if (!IsGrounded ()) {
-
-			float y = rb.velocity.y;
-
-			rb.AddForce (new Vector3 (0, -1.3f, 0));
-
-		} else {
-
-			if (falldownCounter == 0)
-				isJumping = false;
-			else
-				falldownCounter--;
-		}
-	}
-
 
 	/**
 	 * Returns true if player is grounded
@@ -160,16 +151,40 @@ public class NewController : MonoBehaviour {
 	}
 
 	private void Jump2(){
-		if(pressPush)
-			rb.AddForce ( new Vector3(0, 13, 0) );
+		rb.AddForce ( new Vector3(0, GetJumpPower(), 0) );
+	}
+
+	private void FallDown(){
+		rb.AddForce (new Vector3 (0, -1.3f, 0));
+		falldownCounter = 3;
+	}
+
+	private void Landing(){
+
+		if (falldownCounter == 0)
+			isJumping = false;
 		else
-			rb.AddForce ( new Vector3(0, 10, 0) );
+			falldownCounter--;
 	}
 		
+	/**
+	 * Return the jumppower, multiplied if push button is pressed
+	 */
+	public float GetJumpPower(){
+		if (pressPush)
+			return jumpPower * 1.3f;
+		else
+			return jumpPower;
+	}
 
-	private void SetPower(int numFrames, float maxVelocity){
-		numJumpFrames = numFrames;
-		maxVelocity = maxVelocity;
+	/**
+	 * Return the MaxVelocity, multiplied if push button is pressed
+	 */
+	public float GetMaxVelocity(){
+		if (pressPush)
+			return maxVelocity * 1.3f;
+		else
+			return maxVelocity;
 	}
 
 	//Gets the input from the controller and maps it to variables
@@ -180,34 +195,5 @@ public class NewController : MonoBehaviour {
 		pressPush = (Input.GetAxis (prefix + "_Push") == 1) ? true : false;
 		pressThrow = (Input.GetAxis (prefix + "_Throw") == 1) ? true : false;
 
-	}
-
-
-	private void Jumping(){
-
-		if (pressJump && !isJumping) {
-			isJumping = true;
-			StartCoroutine (Jump ());
-		}
-	}
-
-	IEnumerator Jump(){
-
-		int frame = 0;
-
-		numJumpFrames = 5;
-
-		while (pressJump && frame < numJumpFrames) {
-
-			Vector3 force = new Vector3();
-
-			force.y += 1f - 1f * (frame / numJumpFrames);
-			//force.y += 30f - 30f * (frame / numJumpFrames);
-			//force *= Time.deltaTime;
-			rb.AddForce ( force );
-			Debug.Log ("Frame " + frame + " / " + numJumpFrames);
-			frame += 1;
-			yield return new WaitForSeconds (0f);
-		}
 	}
 }
