63cff884087774f1ece81d34f31c0c61b4ee3620
Jeppeh Olsen <jeppeholsen@anon-50-198.vpn.ipredator.se>
2016-03-31 20:43:25 +0200

    Fixed player controller, made water prefab

--- Assets/Scripts/Person/NewController.cs
+++ Assets/Scripts/Person/NewController.cs
@@ -22,7 +22,6 @@ public class NewController : MonoBehaviour {
 	private bool isJumping = false;
 
 	public float accelerationRate = 10;
-	public float maxAcceleration = 5;
 	public float maxVelocity = 1;
 	public float jumpPower = 10;
 
@@ -47,7 +46,6 @@ public class NewController : MonoBehaviour {
 		//Multiply with the game speed
 		maxVelocity *= LevelManager.SPEED;
 		accelerationRate *= LevelManager.SPEED;
-		maxAcceleration *= LevelManager.SPEED;
 	}
 
 	
@@ -62,30 +60,14 @@ public class NewController : MonoBehaviour {
 			GetInputButtonValues ();
 
 			UpdateDirection ();
-
-			//Check if player should throw relic
-			if (pressThrow && this.gameObject.GetComponent<CarriedObject>().isCarrying() == true)
-				Throw ();
-
-			//If explosionCounter is above target, then make explosion
-			if (pressExplode) {
-				Debug.Log ("explision = " + explosionCounter);
-				if (explosionCounter > 200) {
-					
-					//StartCoroutine (ExplodeLayers ());
-					Explode();
-					explosionCounter = 0;
-				}
-			}
-
-			//Charge explosionCounter 
-			explosionCounter++;
+		
 
 			//Move on X and Z axis
 			if (moveHorizontal != 0 || moveVertical != 0)
 				Move ();
 			else
 				AutoRun ();
+			
 
 			//Check if player should fall or land
 			if (!IsGrounded ()) {
@@ -102,12 +84,73 @@ public class NewController : MonoBehaviour {
 
 			if(pressPush)
 				Suicide ();
+
+
+			//Check if player should throw relic
+			if (pressThrow && this.gameObject.GetComponent<CarriedObject>().isCarrying() == true)
+				Throw ();
+
+			//If explosionCounter is above target, then make explosion
+			if (pressExplode) {
+				if (explosionCounter > 200) {
+
+					Explode();
+					//reset counter
+					explosionCounter = 0;
+				}
+			}
+
+			//Charge explosionCounter 
+			explosionCounter++;
+		}
+			
+		LimitWalkingDistance();
+	}
+
+
+	/**
+	 * Add force on X and Z axis based on controller input.
+	 */
+	private void Move(){
+
+		//Get force to rigidbody
+		float hor = moveHorizontal * accelerationRate;
+		float ver = moveVertical * accelerationRate;
+
+		Vector3 force = new Vector3 (hor, 0, ver);
+
+		//Check if player is moving through water
+		if (GetTagOfSurface () == "Water")
+			force /= 4;
+
+		//If player is running towards camera, slow down velocity
+		if (rb.velocity.z < 0) {
+			if (force.z < 0) {
+				force.z /= 4;
+			}
+		}
+			
+		rb.AddForce (force * Time.deltaTime);
+
+		//Make 2D vector to check velocity on X and Y axis
+		Vector2 ground_speed = new Vector2 (rb.velocity.x, rb.velocity.z);
+
+		//If magnitude of x and z velocity is exceeded, then set it to max value.
+		if (ground_speed.magnitude > GetMaxVelocity()) {
+
+			//Set speed to the maxVelocity
+			ground_speed = Vector3.Normalize (ground_speed) * GetMaxVelocity();
+
+			//Convert to 3 dimension vector
+			Vector3 updatedVel = new Vector3 (ground_speed.x, rb.velocity.y, ground_speed.y);
+			rb.velocity = updatedVel;
 		}
 	}
 
 
+
 	private void AutoRun(){
-		rb.AddForce(new Vector3(0,0,0.15f));
+		rb.AddForce( new Vector3(0, 0, 0.15f) );
 	}
 
 
@@ -149,6 +192,46 @@ public class NewController : MonoBehaviour {
 
 
 	/**
+	 * Add upwards force
+	 */
+	private void Jump(){
+
+		float jumpPower = GetJumpPower();
+
+		//Make smaller jump, if on water
+		if (GetTagOfSurface () == "Water")
+			jumpPower /= 3;
+
+		//Add speed on X and Z axis if jumping
+		float hor = moveHorizontal * accelerationRate / 5;
+		float ver = moveVertical * accelerationRate / 5;
+
+		rb.AddForce ( new Vector3(hor, jumpPower, ver) );
+	}
+
+
+	/**
+	 * Add additional downward force to falling
+	 */
+	private void FallDown(){
+		rb.AddForce (new Vector3 (0, -1f, 0));
+		falldownCounter = 1;
+	}
+
+
+	/**
+	 * When a player land, it sets jumping to false, when falldownCounter has decremented to zero
+	 */
+	private void Landing(){
+
+		if (falldownCounter == 0)
+			isJumping = false;
+		else
+			falldownCounter--;
+	}
+
+
+	/**
 	 * Throws the relic and removes it as child
 	 * TODO Check if gameObject is present
 	 */ 
@@ -165,105 +248,40 @@ public class NewController : MonoBehaviour {
 	}
 
 
-	/**
-	 * 	Updates the direction showing the facing of the player.
-	 */
-	private void UpdateDirection (){
-
-		//Update if magnitude is below threshold
-		if(rb.velocity.magnitude > 1f)
-			direction = rb.velocity;
+	public void Suicide(){
+		this.gameObject.GetComponent<PlayerManager> ().Suicide ();
 	}
 
-	public Vector3 GetDirection(){
-		return direction;
-	}
-
-
 	/**
-	 * Add force on X and Z axis based on controller input.
+	 * Sets a limit on how far a character can move on the Z-axis
 	 */
-	private void Move(){
-
-		//Add force to rigidbody
-		float hor = moveHorizontal * accelerationRate;
-		float ver = moveVertical * accelerationRate;
-
-		Vector3 force = new Vector3 (hor, 0, ver);
-
-		if (force.magnitude > maxAcceleration)
-			force = Vector3.Normalize (force) * maxAcceleration;
-	
-		//Get CameraZ position
-		float cameraZ = GameObject.Find ("LeapControllerBlockHand").GetComponent<CameraController> ().GetZPosition ();
-		//float cameraZ = GameObject.Find ("Relic").GetComponent<CameraController> ().GetZPosition ();
-
-		//Only add force if player is behind invisisble wall
-		if (rb.position.z < cameraZ + 3f) {
-
-			rb.AddForce (force * Time.deltaTime);
-		}
-
-
-		//Make 2D vector to check ground velocity
-		Vector2 ground_speed = new Vector2 (rb.velocity.x, rb.velocity.z);
-
-		//If magnitude of x and z velocity is exceeded, then set it to max value.
-		if (ground_speed.magnitude > GetMaxVelocity()) {
-
-			//Set speed to the maxVelocity
-			ground_speed = Vector3.Normalize (ground_speed) * GetMaxVelocity();
-
-			//Convert to 3 dimension vector
-			Vector3 updatedVel = new Vector3 (ground_speed.x, rb.velocity.y, ground_speed.y);
-			rb.velocity = updatedVel;
-		}
-
-
-		//If player is running towards camera, slow down velocity
-		if (rb.velocity.z < 0) {
+	private void LimitWalkingDistance(){
+		if (GetDistanceFromCamera () > 0) {
 			Vector3 vel = rb.velocity;
-			vel.z /= 2;
-			rb.velocity = vel;
+
+			if (vel.z > 0) {
+
+				int divisor = 20;
+				float extra = 0 / divisor;
+
+				//vel.z /= (GetDistanceFromCamera () / 20 + 0.95f);
+				vel.z /= (GetDistanceFromCamera () / divisor + 1);
+				rb.velocity = vel;
+			}
 		}
-			
 	}
 
-
 	/**
-	 * Returns true if player is grounded.
+	 * Returns the distance on the Z-axis from the player to the LEAP
 	 */
-	private bool IsGrounded(){
-		bool isGrounded = Physics.Raycast (player.transform.position, -Vector3.up, 0.4f);//distToGround);
-		return isGrounded;
-	}
-		
-	/**
-	 * Add upwards force
-	 */
-	private void Jump(){
-		rb.AddForce ( new Vector3(0, GetJumpPower(), 0) );
-	}
+	private float GetDistanceFromCamera(){
 
+		//Get Z-position of the LEAP
+		float cameraZ = GameObject.Find ("LeapControllerBlockHand").GetComponent<CameraController> ().GetZPosition ();
 
-	/**
-	 * Add additional downward force to falling
-	 */
-	private void FallDown(){
-		rb.AddForce (new Vector3 (0, -1f, 0));
-		falldownCounter = 3;
-	}
+		float z = rb.position.z - cameraZ;
 
-
-	/**
-	 * When a player land, it sets jumping to false, when falldownCounter has decremented to zero
-	 */
-	private void Landing(){
-
-		if (falldownCounter == 0)
-			isJumping = false;
-		else
-			falldownCounter--;
+		return z;
 	}
 		
 	/**
@@ -300,8 +318,32 @@ public class NewController : MonoBehaviour {
 
 	}
 
-	public void Suicide(){
+	/**
+	 * Shoots a ray down and returns the tag as a string 
+	 * if the ray hits anything. Else it returns null.
+	 */
+	private string GetTagOfSurface(){
+		RaycastHit hit = new RaycastHit();
+		if (Physics.Raycast (rb.position, Vector3.down, out hit, 1f)) {
+			return hit.collider.gameObject.tag;
+		} else {
+			return null;
+		}
+	}
 
-			this.gameObject.GetComponent<PlayerManager> ().Suicide ();
+	public Vector3 GetDirection(){
+		return direction;
+	}
+		
+	private void UpdateDirection (){
+
+		//Update if magnitude is below threshold
+		if(rb.velocity.magnitude > 1f)
+			direction = rb.velocity;
+	}
+		
+	private bool IsGrounded(){
+		bool isGrounded = Physics.Raycast (player.transform.position, -Vector3.up, 0.4f);//distToGround);
+		return isGrounded;
 	}
 }
