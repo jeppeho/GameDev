597e69a768bdacd34b721201b5ec43506397f2d4
Nils Rungholm <nilsrungholmjensen@gmail.com>
2016-04-12 13:37:56 +0200

    Last fixes, I promise <3

--- Assets/Scripts/Game/LevelManager.cs
+++ Assets/Scripts/Game/LevelManager.cs
@@ -5,6 +5,6 @@ public static class LevelManager {
 
 	public static float SPEED = 2.5f;
 
-	public static float MIN_X = 0, MAX_X = 10, MIN_Y = 0, MAX_Y = 30, RELIC_MINZ = 0, MOVE_MINZ = 20, MOVE_MAXZ = 50, MOVE_ZONEWIDTH = 20;
+	public static float MIN_X = 0, MAX_X = 10, MIN_Y = 0, MAX_Y = 30, RELIC_MINZ = 0, MOVE_MINZ = -8, MOVE_MAXZ = 20, MOVE_ZONEWIDTH = 5;
 
 }
