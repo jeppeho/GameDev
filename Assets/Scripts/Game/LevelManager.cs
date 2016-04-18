13dda4845d1f5fdd30dcb66a149cc1a5cf7f6662
Jeppeh Olsen <jeppeholsen@a.local>
2016-04-18 13:28:16 +0200

    Centered everything in Julias prototype

--- Assets/Scripts/Game/LevelManager.cs
+++ Assets/Scripts/Game/LevelManager.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public static class LevelManager {
 
-	public static float SPEED = 20f;
+	//CAMERA SPEED
+	public static float SPEED = 5f;
 
 	public static float MIN_X = 0, MAX_X = 10, MIN_Y = 0, MAX_Y = 30, RELIC_MINZ = -150, MOVE_MINZ = -8, MOVE_MAXZ = 15, MOVE_ZONEWIDTH = 5;
 
