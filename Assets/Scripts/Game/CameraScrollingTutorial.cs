c90e43a6a041358bd8ffe094cfc9a7790786abcf
mogli88 <julianeu@MacBooks-Julia-Lotzow.local>
2016-05-02 17:53:11 +0200

    Update of Tutorial / Start Screen

---
+++ Assets/Scripts/Game/CameraScrollingTutorial.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScrollingTutorial : MonoBehaviour {
+
+	private float cameraSpeed = 2f;
+
+	public GameObject text;
+
+	// Use this for initialization
+	void Start () {
+		text.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		MoveForward ();
+	
+	}
+
+	private void MoveForward(){
+
+		Vector3 currentPosition = this.gameObject.transform.position;
+
+		if(currentPosition.z < -0)
+			{
+			currentPosition.z += cameraSpeed * Time.deltaTime; 
+
+			this.gameObject.transform.position = currentPosition;
+			}
+		else
+		{	
+			text.SetActive(true);
+		}
+	}
+}
